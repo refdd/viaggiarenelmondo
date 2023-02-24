9cf87a4081fcf2df9b5ada1e47465d5175199016
refdd <titorefat76@example.com>
2023-02-24 03:50:06 +0200

    review and nav bar

--- js/main.js
+++ js/main.js
@@ -142,22 +142,22 @@ function removeCounter(type) {
   }
   updateText(adults.value, Children.value);
 }
-// date
-$(function () {
-  $('input[name="daterange"]').daterangepicker(
-    {
-      opens: "left",
-    },
-    function (start, end, label) {
-      console.log(
-        "A new date selection was made: " +
-          start.format("YYYY-MM-DD") +
-          " to " +
-          end.format("YYYY-MM-DD")
-      );
-    }
-  );
-});
+// // date
+// $(function () {
+//   $('input[name="daterange"]').daterangepicker(
+//     {
+//       opens: "left",
+//     },
+//     function (start, end, label) {
+//       console.log(
+//         "A new date selection was made: " +
+//           start.format("YYYY-MM-DD") +
+//           " to " +
+//           end.format("YYYY-MM-DD")
+//       );
+//     }
+//   );
+// });
 
 // counter filter listToura
 // set value of counter
---
+++ js/reviewSlider.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#new-product", {
+    type: "loop",
+    perPage: 2,
+    perMove: 1,
+    gap: "30px",
+    breakpoints: {
+      1024: {
+        perPage: 2,
+      },
+      767: {
+        perPage: 2,
+      },
+      640: {
+        perPage: 1,
+      },
+      autoScroll: {
+        speed: 2,
+      },
+      autoScroll: {
+        speed: 1,
+      },
+    },
+    pagination: false,
+  }).mount(window.splide.Extensions);
+});
