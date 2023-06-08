750355ae812cf8d5506df4cd6bad77ab54eb3289
RefatAli <mohamedrefat8434@gmail.com>
2023-06-08 14:17:14 +0300

    slider reviews

--- js/offerSlider.js
+++ js/offerSlider.js
@@ -1,33 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    new Splide(".splide", {
-        type   : 'loop',
-        perPage: 3, // Number of visible slides
-        gap: "30px", // Gap between slides
-        focus: 'center',
-    
-        pagination: true,
-        breakpoints: {
-        1024: {
-          perPage: 3,
-        },
-        767: {
-          perPage: 2,
-        },
-        640: {
-          perPage: 1,
-        },
-        autoScroll: {
-          speed: 1,
-        },
-  
-        // autoScroll: {
-        //   speed: 1,
-        // },
-      },
-      classes: {
-		pagination: 'splide__pagination your-class-pagination',
-		page      : 'splide__pagination__page your-class-page',
-       },
-    }).mount();
-  });
-  
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
